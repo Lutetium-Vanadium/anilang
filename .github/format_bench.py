3c19173abf1cdb8e7e1ce65c32c2ff7d8f638db6
Lutetium-Vanadium <52347010+Lutetium-Vanadium@users.noreply.github.com>
2021-02-13 19:24:20 +0530

    Benchmarks
    Added benchmarks and a workflow to automatically bench and comment the diff in the form of a table.

---
+++ .github/format_bench.py
@@ -0,0 +1,58 @@
+import sys
+from json import loads as parse_json
+
+def format_change(change):
+    if change == None:
+        return ('-', None)
+    
+    return (
+        'No change' if (a := change['change']) == 'NoChange' else a,
+        change['mean']['estimate']
+    )
+
+def format_signed(num):
+    if num < 0:
+        return f'-{-num}'
+    else:
+        return f'+{num}'
+
+units = ['ns', 'us', 'ms', 's']
+
+def try_bump_unit(num, unit, max='s'):
+    unit_i = units.index(unit)
+    max_i = units.index(max)
+
+    while unit_i < max_i and abs(num) > 1000:
+        unit_i += 1
+        num /= 1000
+
+    return round(num, 2), units[unit_i]
+
+
+
+print('| benchmark | current time | previous time | diff | diff% | change |')
+print('| --------- | ------------ | ------------- | ---- | ----- | ------ |')
+
+for line in sys.stdin:
+    json = parse_json(line)
+    if json['reason'] != 'benchmark-complete':
+        continue
+
+    id = json['id']
+    orig_unit = json['unit']
+    typical_estimate = json['typical']['estimate']
+    cur, unit = try_bump_unit(typical_estimate, orig_unit)
+    change_txt, change = format_change(json['change'])
+
+    print(f'| {id} | {cur} {unit} |', end='')
+
+    if change:
+        previous = typical_estimate * (1 + change)
+
+        prev = try_bump_unit(previous, orig_unit, unit)[0]
+        diff = try_bump_unit(previous - typical_estimate, orig_unit, unit)[0]
+        diff_percent = round(100 * change, 2)
+
+        print(f' {prev} {unit} | {format_signed(diff)} {unit} | {format_signed(diff_percent)}% | {change_txt} |')
+    else:
+        print(' - | - | - | - |')
